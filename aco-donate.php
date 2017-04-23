b14bbb688fbbf0d062ac0481ee84f0831c970295
mahpgnaohhnim <minhhoangpham@live.de>
2017-04-23 19:44:03 +0200

    changed Author URI

--- aco-donate.php
+++ aco-donate.php
@@ -11,5 +11,5 @@ Plugin URI: http://wordpress.org/plugins/hello-dolly/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: mahpgnaohhnim
 Version: 1.0
-Author URI: http://www.asia-charity.de/
+Author URI: https://github.com/mahpgnaohhnim
 */
