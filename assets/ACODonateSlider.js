3ca5f7236cb2736dd2525c26815273462bfa4568
mahpgnaohhnim <minhhoangpham@live.de>
2017-04-24 21:53:24 +0200

    Outsourced Javascript File Add: repositioning Grip and values on window resize

---
+++ assets/ACODonateSlider.js
@@ -0,0 +1,70 @@
+jQuery('document').ready(function(){
+    var grip;
+    var rail;
+    var startX;
+    var railLength;
+    var maxVal;
+    var donationInput;
+    init();
+    window.addEventListener("resize", reset);
+
+    function init(){
+        initSlider();
+        initInput();
+        startX = rail[0].offsetLeft;
+        railLength = rail[0].offsetWidth - grip[0].offsetWidth;
+        maxVal = getMaxDonationValue();
+    }
+
+    function initInput(){
+        donationInput = document.getElementById('donation-input');
+        donationInput.addEventListener('input', onInputChange);
+    }
+
+    function initSlider(){
+        grip = jQuery('#slide-grip').draggable({ containment: 'parent', axis: 'x' });
+        rail = jQuery('#slide-rail');
+        grip.on('drag', onSlideDrag);
+    }
+
+    function onSlideDrag(e){
+        var currentX = e.target.offsetLeft - startX;
+        var percent = currentX/railLength;
+        donationInput.value = Math.round(percent * maxVal);
+
+    }
+
+    function onInputChange(){
+        var inputVal = donationInput.value;
+        if(inputVal !== ''){
+            var percent = inputVal/maxVal;
+            if(percent > 1){
+                percent = 1;
+            }
+            var newXPosition = startX + (percent*railLength);
+            grip.css('left', newXPosition);
+        }
+
+    }
+
+    function getMaxDonationValue(){
+        return (donationInput.max);
+    }
+
+    function reset(){
+        donationInput.removeEventListener('input', onInputChange);
+        grip.off('drag', onSlideDrag);
+        grip.draggable("destroy");
+        adjustGripTopPosition();
+        init();
+        onInputChange();
+    }
+
+    function adjustGripTopPosition(){
+        var gripY = grip.css('top');
+        var railY = rail[0].offsetTop;1
+        if(gripY !== railY){
+            grip.css('top', railY);
+        }
+    }
+});
